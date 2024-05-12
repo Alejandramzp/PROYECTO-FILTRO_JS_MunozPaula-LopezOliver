d1779bd3da13e0d40bc3aecb42c437c11390605d
Oli <oliverlopez42069@gmail.com>
2024-05-12 11:21:35 -0500

    feat: :hammer: Asignar clase a textos
    Se le asignó una clase general temporal a los textos para configurar su fuente y color

--- js/script.js
+++ js/script.js
@@ -20,41 +20,45 @@ function displayRocketInfo(r) {
             const rocket = data[r];
             if (rocket) {
                 document.getElementById("info").innerHTML = `
-                    <h1>${rocket.name}</h1>
-                    <p><strong>ID:</strong> ${rocket.id}</p>
-                    <p><strong>Type:</strong> ${rocket.type}</p>
-                    <p><strong>Active:</strong> ${rocket.active}</p>
-                    <p><strong>Stages:</strong> ${rocket.stages}</p>
-                    <p><strong>Boosters:</strong> ${rocket.boosters}</p>
-                    <p><strong>Cost per launch:</strong> ${rocket.cost_per_launch}</p>
-                    <p><strong>Success rate pct:</strong> ${rocket.success_rate_pct}</p>
-                    <p><strong>First flight:</strong> ${rocket.first_flight}</p>
-                    <p><strong>Country:</strong> ${rocket.country}</p>
-                    <p><strong>Company:</strong> ${rocket.company}</p>
-                    <p><strong>Description:</strong> ${rocket.description}</p>
-                    <a href="${rocket.wikipedia}">wikipedia</a><br>
+                <div class="textoinf">
+                <h1 class="rname">${rocket.name}</h1>
+                <p class=""><strong>ID:</strong> ${rocket.id}</p>
+                <p class=""><strong>Type:</strong> ${rocket.type}</p>
+                <p class=""><strong>Active:</strong> ${rocket.active}</p>
+                <p class=""><strong>Stages:</strong> ${rocket.stages}</p>
+                <p class=""><strong>Boosters:</strong> ${rocket.boosters}</p>
+                <p class=""><strong>Cost per launch:</strong> ${rocket.cost_per_launch}</p>
+                <p class=""><strong>Success rate pct:</strong> ${rocket.success_rate_pct}</p>
+                <p class=""><strong>First flight:</strong> ${rocket.first_flight}</p>
+                <p class=""><strong>Country:</strong> ${rocket.country}</p>
+                <p class=""><strong>Company:</strong> ${rocket.company}</p>
+                <p class=""><strong>Description:</strong> ${rocket.description}</p>
+                <a href="${rocket.wikipedia}">wikipedia</a><br>
 
-                //----------------------MEDIDAS------------------
-                    <p><strong>Height:</strong> ${rocket.height.meters} meters - ${rocket.height.feet} feet</p>
-                    <p><strong>Diameter:</strong> ${rocket.diameter.meters} meters - ${rocket.diameter.feet} feet</p>
-                    <p><strong>Mass:</strong> ${rocket.mass.kg} kg - ${rocket.mass.lb} lb </p>
+            //----------------------MEDIDAS------------------
+                <p><strong>Height:</strong> ${rocket.height.meters} meters - ${rocket.height.feet} feet</p>
+                <p><strong>Diameter:</strong> ${rocket.diameter.meters} meters - ${rocket.diameter.feet} feet</p>
+                <p><strong>Mass:</strong> ${rocket.mass.kg} kg - ${rocket.mass.lb} lb </p>
 
-                //------------------PRIMER ESCENARIO---------
-                    <p><strong>First stage:</strong></p>
-                    <p><strong>Thrust sea level:</strong> ${rocket.first_stage.thrust_sea_level.kN} kN - ${rocket.first_stage.thrust_sea_level.lbf} lbf </p>
-                    <p><strong>Thrust vacuum:</strong> ${rocket.first_stage.thrust_vacuum.kN} kN - ${rocket.first_stage.thrust_vacuum.lbf} lbf </p>
-                    <p><strong>Reusable:</strong> ${rocket.reusable}</p>
-                    <p><strong>Engines:</strong> ${rocket.engines}</p>
-                    <p><strong>Fuel amount tons:</strong> ${rocket.fuel_amount_tons}</p>
-                    <p><strong>Burn time sec:</strong> ${rocket.burn_time_sec}</p>
+            //------------------PRIMER ESCENARIO---------
+                <p><strong>First stage:</strong></p>
+                <p><strong>Thrust sea level:</strong> ${rocket.first_stage.thrust_sea_level.kN} kN - ${rocket.first_stage.thrust_sea_level.lbf} lbf </p>
+                <p><strong>Thrust vacuum:</strong> ${rocket.first_stage.thrust_vacuum.kN} kN - ${rocket.first_stage.thrust_vacuum.lbf} lbf </p>
+                <p><strong>Reusable:</strong> ${rocket.reusable}</p>
+                <p><strong>Engines:</strong> ${rocket.engines}</p>
+                <p><strong>Fuel amount tons:</strong> ${rocket.fuel_amount_tons}</p>
+                <p><strong>Burn time sec:</strong> ${rocket.burn_time_sec}</p>
 
-                //------------------SEGUNDO ESCENARIO---------
-                <p><strong>Second stage:</strong></p>
-                <p><strong>Thrust:</strong> ${rocket.second_stage.thrust.kN} kN - ${rocket.second_stage.thrust.lbf} lbf </p>
-                <p><strong>Payloads:</strong></p>
-                <p><strong>Composite fairing:</strong></p>
-                <p><strong>Height:</strong> ${rocket.second_stage.payloads.composite_fairing.height.meters} meters - ${rocket.second_stage.payloads.composite_fairing.height.feet} feet</p>
-                `;
+            //------------------SEGUNDO ESCENARIO---------
+            <p><strong>Second stage:</strong></p>
+            <p><strong>Thrust:</strong> ${rocket.second_stage.thrust.kN} kN - ${rocket.second_stage.thrust.lbf} lbf </p>
+            <p><strong>Payloads:</strong></p>
+            <p><strong>Composite fairing:</strong></p>
+            <p><strong>Height:</strong> ${rocket.second_stage.payloads.composite_fairing.height.meters} meters - ${rocket.second_stage.payloads.composite_fairing.height.feet} feet</p>
+            
+                
+                </div>
+                    `;
             }
         })
         .catch((error) => console.log(error));
