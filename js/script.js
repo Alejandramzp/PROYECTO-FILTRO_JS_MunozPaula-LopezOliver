a1ac247804ac2b9ff26a9de7eee8bf358261f722
Oli <oliverlopez42069@gmail.com>
2024-05-13 22:45:59 -0500

    feat: :beers: Re-ordenar estructura elementos
    Se re-acomodo la estructura de los elementos para que esta coincida con la propuesta por el framework

--- js/script.js
+++ js/script.js
@@ -20,73 +20,67 @@ function displayRocketInfo(r) {
             const rocket = data[r];
             if (rocket) {
                 document.getElementById("infoRockets").innerHTML = `
-                <div class="infoPrincipal">
-                    <h1 class="rname">${rocket.name}</h1>
-                    <p class=""><strong>ID:</strong> ${rocket.id}</p>
-                    <p class=""><strong>Type:</strong> ${rocket.type}</p>
-                    <p class=""><strong>Active:</strong> ${rocket.active}</p>
-                    <p class=""><strong>Stages:</strong> ${rocket.stages}</p>
-                    <p class=""><strong>Boosters:</strong> ${rocket.boosters}</p>
-                    <p class=""><strong>Cost per launch:</strong> ${rocket.cost_per_launch}</p>
-                    <p class=""><strong>Success rate pct:</strong> ${rocket.success_rate_pct}</p>
-                    <p class=""><strong>First flight:</strong> ${rocket.first_flight}</p>
-                    <p class=""><strong>Country:</strong> ${rocket.country}</p>
-                    <p class=""><strong>Company:</strong> ${rocket.company}</p>
-                    <p class=""><strong>Description:</strong> ${rocket.description}</p>
-                    <a href="${rocket.wikipedia}">wikipedia</a><br>
+                <h1 class="rname">${rocket.name}</h1>
+                <p class="rid"><strong>ID:</strong> ${rocket.id}</p>
+                <p class="rdesc"><strong>Description:</strong><br><span style="color: #787878;"> ${rocket.description}</p>
+                <a href="${rocket.wikipedia}" target="_blank">
+                <img src="/storage/images/Wikipedia_Logo_1.0.png" class="rwiki">
+                </a>
+                <div class="generalinfo">
+                    <p><strong>Type:</strong><br><span style="color: #787878;">${rocket.type}</span></p>
+                    <p ><strong>Active:</strong><br><span style="color: #787878;"> ${rocket.active}</p>
+                    <p ><strong>Stages:</strong><br><span style="color: #787878;"> ${rocket.stages}</p>
+                    <p ><strong>Boosters:</strong><br><span style="color: #787878;"> ${rocket.boosters}</p>
+                    <p ><strong>Cost per launch:</strong><br><span style="color: #787878;"> ${rocket.cost_per_launch}</p>
+                    <p ><strong>Success rate pct:</strong><br><span style="color: #787878;"> ${rocket.success_rate_pct}</p>
+                    <p ><strong>First flight:</strong><br><span style="color: #787878;"> ${rocket.first_flight}</p>
+                    <p ><strong>Country:</strong><br><span style="color: #787878;"> ${rocket.country}</p>
+                    <p ><strong>Company:</strong><br><span style="color: #787878;"> ${rocket.company}</p>
                 </div>
-            //----------------------MEDIDAS------------------
-                <div class="medidas">
-                    <p><strong>Height:</strong> ${rocket.height.meters} meters - ${rocket.height.feet} feet</p>
-                    <p><strong>Diameter:</strong> ${rocket.diameter.meters} meters - ${rocket.diameter.feet} feet</p>
-                    <p><strong>Mass:</strong> ${rocket.mass.kg} kg - ${rocket.mass.lb} lb </p>
+                <div class="advancedinfo">
+                    <p><strong>Height:</strong><br><span style="color: #787878;"> ${rocket.height.meters} meters<br>${rocket.height.feet} feet</p>
+                    <p><strong>Diameter:</strong><br><span style="color: #787878;"> ${rocket.diameter.meters} meters<br>${rocket.diameter.feet} feet</p>
+                    <p><strong>Mass:</strong><br><span style="color: #787878;">  ${rocket.mass.kg} kg<br> ${rocket.mass.lb} lb </p>
+                    <p><strong>Landing legs:</strong></p>
+                    <p><strong>Number:</strong><br><span style="color: #787878;"> ${rocket.landing_legs.number}</p>
+                    <p><strong>Material:</strong><br><span style="color: #787878;"> ${rocket.landing_legs.material}</p>
                 </div>
-            //------------------PRIMER ESCENARIO---------
-                <div class="primerEscenario">
+                <div class="stages">
                     <p><strong>First stage:</strong></p>
-                    <p><strong>Thrust sea level:</strong> ${rocket.first_stage.thrust_sea_level.kN} kN - ${rocket.first_stage.thrust_sea_level.lbf} lbf </p>
-                    <p><strong>Thrust vacuum:</strong> ${rocket.first_stage.thrust_vacuum.kN} kN - ${rocket.first_stage.thrust_vacuum.lbf} lbf </p>
-                    <p><strong>Reusable:</strong> ${rocket.first_stage.reusable}</p>
-                    <p><strong>Engines:</strong> ${rocket.first_stage.engines}</p>
-                    <p><strong>Fuel amount tons:</strong> ${rocket.first_stage.fuel_amount_tons}</p>
-                    <p><strong>Burn time sec:</strong> ${rocket.first_stage.burn_time_sec}</p>
-                </div>
-            //------------------SEGUNDO ESCENARIO---------
-                <div class="segundoEscenario">
+                    <p><strong>Thrust sea level:</strong><br><span style="color: #787878;"> ${rocket.first_stage.thrust_sea_level.kN} kN<br> ${rocket.first_stage.thrust_sea_level.lbf} lbf </p>
+                    <p><strong>Thrust vacuum:</strong><br><span style="color: #787878;"> ${rocket.first_stage.thrust_vacuum.kN} kN<br> ${rocket.first_stage.thrust_vacuum.lbf} lbf </p>
+                    <p><strong>Reusable:</strong><br><span style="color: #787878;"> ${rocket.first_stage.reusable}</p>
+                    <p><strong>Engines:</strong><br><span style="color: #787878;"> ${rocket.first_stage.engines}</p>
+                    <p><strong>Fuel amount tons:</strong><br><span style="color: #787878;"> ${rocket.first_stage.fuel_amount_tons}</p>
+                    <p><strong>Burn time sec:</strong><br><span style="color: #787878;"> ${rocket.first_stage.burn_time_sec}</p>
+                    
                     <p><strong>Second stage:</strong></p>
-                    <p><strong>Thrust:</strong> ${rocket.second_stage.thrust.kN} kN - ${rocket.second_stage.thrust.lbf} lbf </p>
+                    <p><strong>Thrust:</strong><br><span style="color: #787878;"> ${rocket.second_stage.thrust.kN} kN<br> ${rocket.second_stage.thrust.lbf} lbf </p>
                     <p><strong>Payloads:</strong></p>
                     <p><strong>Composite fairing:</strong></p>
-                    <p><strong>Height:</strong> ${rocket.second_stage.payloads.composite_fairing.height.meters} meters - ${rocket.second_stage.payloads.composite_fairing.height.feet} feet</p>
-                    <p><strong>Diameter:</strong> ${rocket.second_stage.payloads.composite_fairing.diameter.meters} meters - ${rocket.second_stage.payloads.composite_fairing.diameter.feet} feet</p>
-                    <p><strong>Option 1:</strong> ${rocket.second_stage.payloads.option_1}</p>
-                    <p><strong>Reusable:</strong> ${rocket.second_stage.reusable}</p>
-                    <p><strong>Engines:</strong> ${rocket.second_stage.engines}</p>
-                    <p><strong>Fuel amount tons:</strong> ${rocket.second_stage.fuel_amount_tons}</p>
-                    <p><strong>Burn time sec:</strong> ${rocket.second_stage.burn_time_sec}</p>
+                    <p><strong>Height:</strong><br><span style="color: #787878;"> ${rocket.second_stage.payloads.composite_fairing.height.meters} meters<br> ${rocket.second_stage.payloads.composite_fairing.height.feet} feet</p>
+                    <p><strong>Diameter:</strong><br><span style="color: #787878;"> ${rocket.second_stage.payloads.composite_fairing.diameter.meters} meters<br> ${rocket.second_stage.payloads.composite_fairing.diameter.feet} feet</p>
+                    <p><strong>Option 1:</strong><br><span style="color: #787878;"> ${rocket.second_stage.payloads.option_1}</p>
+                    <p><strong>Reusable:</strong><br><span style="color: #787878;"> ${rocket.second_stage.reusable}</p>
+                    <p><strong>Engines:</strong><br><span style="color: #787878;"> ${rocket.second_stage.engines}</p>
+                    <p><strong>Fuel amount tons:</strong><br><span style="color: #787878;"> ${rocket.second_stage.fuel_amount_tons}</p>
+                    <p><strong>Burn time sec:</strong><br><span style="color: #787878;"> ${rocket.second_stage.burn_time_sec}</p>
                 </div>
-            //-----------------------ENGINES-----------------
                 <div class="engines">
                     <p><strong>Engines:</strong></p>
                     <p><strong>Isp:</strong></p>
-                    <p><strong>Sea level:</strong> ${rocket.engines.isp.sea_level}</p>
-                    <p><strong>Vacuum:</strong> ${rocket.engines.isp.vacuum}</p>
-                    <p><strong>Thrust sea level:</strong> ${rocket.engines.thrust_sea_level.kN} kN - ${rocket.engines.thrust_sea_level.lbf} lbf</p>
-                    <p><strong>Thrust vacuum:</strong> ${rocket.engines.thrust_vacuum.kN} kN - ${rocket.engines.thrust_vacuum.lbf} lbf</p>
-                    <p><strong>Number:</strong> ${rocket.engines.number}</p>
-                    <p><strong>Type:</strong> ${rocket.engines.type}</p>
-                    <p><strong>Version:</strong> ${rocket.engines.version}</p>
-                    <p><strong>Layout:</strong> ${rocket.engines.layout}</p>
-                    <p><strong>Engine loss max:</strong> ${rocket.engines.engine_loss_max}</p>
-                    <p><strong>Propellant 1:</strong> ${rocket.engines.propellant_1}</p>
-                    <p><strong>Propellant 2:</strong> ${rocket.engines.propellant_2}</p>
-                    <p><strong>Thrust to weight:</strong> ${rocket.engines.thrust_to_weight}</p>
-                </div>
-            //---------------------LANDING LEGS-------------------
-                <div class="landingLegs">
-                    <p><strong>Landing legs:</strong></p>
-                    <p><strong>Number:</strong> ${rocket.landing_legs.number}</p>
-                    <p><strong>Material:</strong> ${rocket.landing_legs.material}</p>
+                    <p><strong>Sea level:</strong><br><span style="color: #787878;"> ${rocket.engines.isp.sea_level}</p>
+                    <p><strong>Vacuum:</strong><br><span style="color: #787878;"> ${rocket.engines.isp.vacuum}</p>
+                    <p><strong>Thrust sea level:</strong><br><span style="color: #787878;"> ${rocket.engines.thrust_sea_level.kN} kN <br> ${rocket.engines.thrust_sea_level.lbf} lbf</p>
+                    <p><strong>Thrust vacuum:</strong><br><span style="color: #787878;"> ${rocket.engines.thrust_vacuum.kN} kN <br> ${rocket.engines.thrust_vacuum.lbf} lbf</p>
+                    <p><strong>Number:</strong><br><span style="color: #787878;"> ${rocket.engines.number}</p>
+                    <p><strong>Type:</strong><br><span style="color: #787878;"> ${rocket.engines.type}</p>
+                    <p><strong>Version:</strong><br><span style="color: #787878;"> ${rocket.engines.version}</p>
+                    <p><strong>Layout:</strong><br><span style="color: #787878;"> ${rocket.engines.layout}</p>
+                    <p><strong>Engine loss max:</strong><br><span style="color: #787878;"> ${rocket.engines.engine_loss_max}</p>
+                    <p><strong>Propellant 1:</strong><br><span style="color: #787878;"> ${rocket.engines.propellant_1}</p>
+                    <p><strong>Propellant 2:</strong><br><span style="color: #787878;"> ${rocket.engines.propellant_2}</p>
+                    <p><strong>Thrust to weight:</strong><br><span style="color: #787878;"> ${rocket.engines.thrust_to_weight}</p>
                 </div>
                 `;
                 //---------------------PAYLOAD WEIGHTS-------------------
@@ -95,10 +89,10 @@ function displayRocketInfo(r) {
                     document.getElementById("infoRockets").innerHTML += `
                     <div class="payloadWeights">
                         <p><strong>Payload weights:</strong></p>
-                        <p><strong>ID:</strong> ${element.id}</p>
-                        <p><strong>Name:</strong> ${element.name}</p>
-                        <p><strong>Kg:</strong> ${element.kg}</p>
-                        <p><strong>Lb:</strong> ${element.lb}</p>
+                        <p><strong>ID:</strong><br><span style="color: #787878;"> ${element.id}</p>
+                        <p><strong>Name:</strong><br><span style="color: #787878;"> ${element.name}</p>
+                        <p><strong>Kg:</strong><br><span style="color: #787878;"> ${element.kg}</p>
+                        <p><strong>Lb:</strong><br><span style="color: #787878;"> ${element.lb}</p>
                     </div>
                     `;
                 });
