7ec592aa7b0f1949cdc1ba0fc22a9ea0afa92654
Paula <alejandrapmp17@gmail.com>
2024-05-15 08:44:39 -0500

    style: :lipstick: Se organizo la informacion de company

--- js/company.js
+++ js/company.js
@@ -4,32 +4,86 @@ fetch(companyUrl)
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        let companyInfo = document.getElementById("infoCompany");
+        let companyInfo = document.getElementById("name");
         companyInfo.innerHTML =`
-        <h2>${data.name}</h2>
-        <p class=""><strong>Founder:</strong> ${data.founder}</p>
-        <p class=""><strong>Founded:</strong> ${data.founded}</p>
-        <p class=""><strong>Employees:</strong> ${data.employees}</p>
-        <p class=""><strong>Vehicles:</strong> ${data.vehicles}</p>
-        <p class=""><strong>Launch sites:</strong> ${data.launch_sites}</p>
-        <p class=""><strong>Test sites:</strong> ${data.test_sites}</p>
-        <p class=""><strong>CEO:</strong> ${data.ceo}</p>
-        <p class=""><strong>CTO:</strong> ${data.cto}</p>
-        <p class=""><strong>COO:</strong> ${data.coo}</p>
-        <p class=""><strong>CTO Propulsion:</strong> ${data.cto_propulsion}</p>
-        <p class=""><strong>Valuation:</strong> ${data.valuation}</p>
-        <p class=""><strong>Summary:</strong> ${data.summary}</p>
-        <p class=""><strong>ID:</strong> ${data.id}</p>
-        <p class=""><strong>Address:</strong> ${data.headquarters.address}</p>
-        <p class=""><strong>City:</strong> ${data.headquarters.city}
-        <p class=""><strong>State:</strong> ${data.headquarters.state}
-        <br><a href="${data.links.website}">Website</a><br>
-        <a href="${data.links.flickr}">Flickr</a><br>
-        <a href="${data.links.twitter}">Twitter</a><br>
-        <a href="${data.links.elon_twitter}">Elon Twitter</a><br>
+        <h2 class="name">${data.name}</h2>
         `;
-            
-
+        document.getElementById("info1").innerHTML=`
+        <div>
+            <span><img class="f1" src="/storage/f2.png"> Founder:</span>
+            <p>${data.founder}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f1.png"> Founded:</span>
+            <p>${data.founded}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f1.png"> Employees:</span>
+            <p>${data.employees}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f1.png"> Vehicles:</span>
+            <p>${data.vehicles}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f1.png"> Launch sites:</span>
+            <p>${data.launch_sites}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f1.png"> Test sites:</span>
+            <p>${data.test_sites}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f3.png"> CEO:</span>
+            <p>${data.ceo}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f1.png"> CTO:</span>
+            <p>${data.cto}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f1.png"> COO:</span>
+            <p>${data.coo}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f2.png"> CTO Propulsion:</span>
+            <p>${data.cto_propulsion}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f1.png"> Valuation:</span>
+            <p>${data.valuation}</p>
+        </div>
+        
+        `;
+        document.getElementById("info2").innerHTML=`
+        <div>
+            <span><img class="f1" src="/storage/f1.png"> Summary:</span>
+            <p>${data.summary}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f2.png"> ID:</span>
+            <p>${data.id}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f1.png"> Address:</span>
+            <p>${data.headquarters.address}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f1.png"> City:</span>
+            <p> ${data.headquarters.city}</p>
+        </div>
+        <div>
+            <span><img class="f1" src="/storage/f3.png"> State:</span>
+            <p>${data.headquarters.state}</p>
+        </div>
+        `;
+        document.getElementById("redes").innerHTML=`
+        <a href="${data.links.flickr}"><img class="ph" src="/storage/images/photos.png"></a><br>
+        <a href="${data.links.elon_twitter}"><img class="tw" src="/storage/images/twitter.png"></a><br>
+        `;
+        document.getElementById("frame").innerHTML=`
+            <iframe class="web" src="${data.links.website}" frameborder="0"></iframe> 
+        `;
     })
     .catch((error) => console.log(error));
 
