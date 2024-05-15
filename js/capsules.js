4bc82dbabaafe30e533049b77510f7ab02edea4d
Paula <alejandrapmp17@gmail.com>
2024-05-15 00:36:58 -0500

    style: :lipstick: Se organizo la informacion de capsules

--- js/capsules.js
+++ js/capsules.js
@@ -8,7 +8,7 @@ fetch(capsulesUrl)
             let capsulesInfo = document.getElementById("infoCapsules");
             capsulesInfo.innerHTML =``;
            
-        });//displayCapsuleInfo(0);
+        });displayCapsuleInfo(0);
     })
     .catch((error) => console.log(error));
     
@@ -19,17 +19,35 @@ function displayCapsuleInfo(c){
        .then(data => {
             const capsule = data[c];
             if (capsule) {
+                document.getElementById("serial").innerHTML = `
+                    <h1>${capsule.serial}</h1>
+                    <p><strong>ID:</strong>${capsule.id}</p>
+                `;
                 document.getElementById("infoCapsules").innerHTML = `
-                <div class="infoPrincipal">
-                    <p class=""><strong>ID:</strong> ${capsule.id}</p>
-                    <p class=""><strong>Type:</strong> ${capsule.type}</p>
-                    <p class=""><strong>Status:</strong> ${capsule.status}</p>
-                    <p class=""><strong>Serial:</strong> ${capsule.serial}</p>
-                    <p class=""><strong>Reuse count:</strong> ${capsule.reuse_count}</p>
-                    <p class=""><strong>Water landings:</strong> ${capsule.water_landings}</p>
-                    <p class=""><strong>Land landings: </strong> ${capsule.land_landings}</p>
-                    <p class=""><strong>Last update:</strong> ${capsule.last_update}</p>
+                <div>
+                    <span><img class="f1" src="/storage/f1.png"> Type:</span>
+                    <p>${capsule.type}</p>
                 </div>
+                <div>
+                    <span><img class="f1" src="/storage/f1.png"> Status:</span>
+                    <p>${capsule.status}</p>
+                </div>
+                <div>
+                    <span><img class="f1" src="/storage/f2.png"> Water landings:</span>
+                    <p>${capsule.water_landings}</p>
+                </div>
+                <div>
+                    <span><img class="f1" src="/storage/f1.png"> Reuse count:</span>
+                    <p>${capsule.reuse_count}</p>
+                </div>
+                <div>
+                    <span><img class="f1" src="/storage/f3.png"> Land landings:</span>
+                    <p>${capsule.land_landings}</p>
+                </div>
+                `;
+                document.getElementById("last").innerHTML=`
+                    <span><img class="f1" src="/storage/satelite.png"> Last update:</span>
+                    <p>${capsule.last_update}</p>    
                 `;
                 launchesId = capsule.launches[0];
                 launchesUrl = `https://api.spacexdata.com/v4/launches/${launchesId}`;
@@ -37,25 +55,24 @@ function displayCapsuleInfo(c){
                 .then(response => response.json())
                 .then(data2 => {
                     //--------------LANZAMIENTO--------------
-                    document.getElementById("infoCapsules").innerHTML += `
-                    <div class="lanzamiento">
-                        <h2 class="">${data2.name}</h2>
-                        <p class=""><strong>ID:</strong> ${data2.id}</p>
-                        <p class=""><strong>patch:</strong></p>
-                        <img src="${data2.links.patch.small}"><br>
-                        <a href="${data2.links.webcast}">Youtube</a><br>
-                        <a href="${data2.links.wikipedia}">Wikipedia</a><br> 
-                        <p class=""><strong>Static fire date utc:</strong> ${data2.static_fire_date_utc}</p>
-                        <p class=""><strong>Static fire date unix:</strong> ${data2.static_fire_date_unix}</p>
+                    document.getElementById("patch").innerHTML=`
+                        <img src="${data2.links.patch.small}">
+                    `;
+                    document.getElementById("lanza1").innerHTML += `
+                        <span><img class="l1" src="/storage/lanzamiento1.png"><strong>Launches:</strong></span>
+                        <p class=""><strong>Name:</strong> ${data2.name}</p>
                         <p class=""><strong>Window:</strong> ${data2.window}</p>
                         <p class=""><strong>Success:</strong> ${data2.success}</p>
+                    `;
+                    document.getElementById("lanza2").innerHTML += `	
+                        <span><img class="l1" src="/storage/lanzamiento2.png"><strong>Launches:</strong></span>
                         <p class=""><strong>Flight number:</strong> ${data2.flight_number}</p>
-                        <p class=""><strong>Date utc:</strong> ${data2.date_utc}</p>
-                        <p class=""><strong>Date unix:</strong> ${data2.date_unix}</p>
                         <p class=""><strong>Date local:</strong> ${data2.date_local}</p>
                         <p class=""><strong>Date precision:</strong> ${data2.date_precision}</p>
-                        <p class=""><strong>Upcoming:</strong> ${data2.upcoming}</p>
-                    </div>
+                    `;
+                    document.getElementById("enlaces").innerHTML=`
+                    <a href="${data2.links.webcast}"><img class="youtube" src="/storage/images/youtube.png"></a><br>
+                    <a href="${data2.links.wikipedia}"><img class="wiki" src="/storage/images/Wikipedia_Logo_1.0.png"></a><br> 
                     `;
                 })
                 .catch((error) => console.log(error)); 
