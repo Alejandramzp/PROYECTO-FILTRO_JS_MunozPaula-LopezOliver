d0fad2ac9b46b98d3ae3b67e582e9228a3f0ed86
Oliwashere <156392065+Oliwashere@users.noreply.github.com>
2024-05-15 13:28:45 -0500

    feat: 🔨 Ajustar estructura api
    Se re-estructuraron los elementos consumidos de la api

--- js/history.js
+++ js/history.js
@@ -7,7 +7,7 @@ fetch(historyUrl)
         data.forEach(historyData => {
             let historyInfo = document.getElementById("infoHistory");
             historyInfo.innerHTML =``;
-            //displayHistoryInfo(0);
+            displayHistoryInfo(0);
         });
     })
     .catch((error) => console.log(error));
@@ -21,13 +21,13 @@ function displayHistoryInfo(h) {
             if (history) {
                 document.getElementById("infoHistory").innerHTML = `
                 <div class="infoPrincipal">
-                    <h2 class="">${history.title}</h2>
-                    <p class=""><strong>ID:</strong> ${history.id}</p>
-                    <p class=""><strong>Event date utc:</strong> ${history.event_date_utc}</p>
-                    <p class=""><strong>Event date unix:</strong> ${history.event_date_unix}</p>
-                    <p class=""><strong>Details:</strong> ${history.details}</p>
-                    <a href="${history.links.article}">Article</a><br>
+                    <h2 class="htitle">${history.title}</h2><br><br><br><br><br>
+                    <p class="hid"><strong>ID:</strong><br><span style="color: grey;">${history.id}</p><br><br>
+                    <p class="hd1"><strong>Event date utc:</strong><br><span style="color: grey;">${history.event_date_utc}</p><br><br>
+                    <p class="hd2"><strong>Event date unix:</strong><br><span style="color: grey;">${history.event_date_unix}</p><br><br>
+                    <p class="hdetails"><strong>Details:</strong><br><span style="color: grey;">${history.details}</p><br><br>
                 </div>
+                <iframe class="articulo" src="${history.links.article}" frameborder="0"></iframe>
                 `;
             }
         })
@@ -37,21 +37,22 @@ function displayHistoryInfo(h) {
 class history15 extends HTMLElement{
     constructor(){
         super();
-        this.innerHTML =`<button id="botonHistory" onclick="displayHistoryInfo(0)">1</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(1)">2</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(2)">3</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(3)">4</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(4)">5</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(5)">6</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(6)">7</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(7)">8</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(8)">9</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(9)">10</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(10)">11</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(11)">12</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(12)">13</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(13)">14</button>
-        <button id="botonHistory" onclick="displayHistoryInfo(14)">15</button>
+        this.innerHTML =`
+        <button class="btn1" id="botonHistory" onclick="displayHistoryInfo(0)">1</button>
+        <button class="btn2" id="botonHistory" onclick="displayHistoryInfo(1)">2</button>
+        <button class="btn3" id="botonHistory" onclick="displayHistoryInfo(2)">3</button>
+        <button class="btn4" id="botonHistory" onclick="displayHistoryInfo(3)">4</button>
+        <button class="btn5" id="botonHistory" onclick="displayHistoryInfo(4)">5</button>
+        <button class="btn6" id="botonHistory" onclick="displayHistoryInfo(5)">6</button>
+        <button class="btn7" id="botonHistory" onclick="displayHistoryInfo(6)">7</button>
+        <button class="btn8" id="botonHistory" onclick="displayHistoryInfo(7)">8</button>
+        <button class="btn9" id="botonHistory" onclick="displayHistoryInfo(8)">9</button>
+        <button class="btn10" id="botonHistory" onclick="displayHistoryInfo(9)">10</button>
+        <button class="btn11" id="botonHistory" onclick="displayHistoryInfo(10)">11</button>
+        <button class="btn12" id="botonHistory" onclick="displayHistoryInfo(11)">12</button>
+        <button class="btn13" id="botonHistory" onclick="displayHistoryInfo(12)">13</button>
+        <button class="btn14" id="botonHistory" onclick="displayHistoryInfo(13)">14</button>
+        <button class="btn15" id="botonHistory" onclick="displayHistoryInfo(14)">15</button>
         `;
     }    
 }
